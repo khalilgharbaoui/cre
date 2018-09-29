2c036a2675436284c4402ad24308868cb21a6612
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-09-29 13:01:28 +0200

    Add cre code

---
+++ lib/cre/version.rb
@@ -0,0 +1,3 @@
+module Cre
+  VERSION = "0.1.0"
+end
---
+++ spec/cre_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Cre do
+  it "has a version number" do
+    expect(Cre::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
